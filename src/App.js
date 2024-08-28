5651f8b7d15f52b34cc544309eff9f5aeb8cfb5b
PoojaChinu <166599982+PoojaChinu@users.noreply.github.com>
2024-08-28 12:41:20 +0200

    Adding styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import { extractLocations, getEvents } from "./api";
 import NumberOfEvents from "./components/NumberOfEvents";
+import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -28,9 +32,19 @@ function App() {
 
   return (
     <div className="App">
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
+      </div>
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <NumberOfEvents
+        setNumberOfEvents={setCurrentNOE}
+        setInfoAlert={setInfoAlert}
+        setErrorAlert={setErrorAlert}
+        setWarningAlert={setWarningAlert}
+      />
       <EventList events={events} />
-      <NumberOfEvents setNumberOfEvents={setCurrentNOE} />
     </div>
   );
 }
