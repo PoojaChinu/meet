f0cca257e63637d4649a62c245ee8b203fe2955e
PoojaChinu <166599982+PoojaChinu@users.noreply.github.com>
2024-09-05 10:47:22 +0200

    Loading Cached Event Data

--- src/api.js
+++ src/api.js
@@ -23,10 +23,13 @@ const checkToken = async (accessToken) => {
 };
 
 export const getEvents = async () => {
-  if (window.location.href.startsWith("http://localhost")) return mockData;
-
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }
   if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
+    NProgress.done();
     return events ? JSON.parse(events) : [];
   }
 
@@ -41,6 +44,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
       localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
